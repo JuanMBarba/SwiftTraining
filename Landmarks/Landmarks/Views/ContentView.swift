7cdeaca4245c7ce38366bd7f972e915badabb670
Juan Barba <barbajuan11@gmail.com>
2022-11-10 15:21:56 -0800

    Saving files for past commit

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Juan Barba on 11/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
