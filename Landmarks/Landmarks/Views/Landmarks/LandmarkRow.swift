9088789fe0c74f78783a4f3b452d16f92efc158a
Juan Barba <barbajuan11@gmail.com>
2022-11-11 10:04:28 -0800

    Updated Landmarks project to incorporate user input that changes the state of the app

--- Landmarks/Landmarks/Views/Landmarks/LandmarkRow.swift
+++ Landmarks/Landmarks/Views/Landmarks/LandmarkRow.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct LandmarkRow: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
+    
+//    var landmarkIndex: Int {
+//        modelData.landmarks.firstIndex(where: {$0.id == landmark.id})!
+//    }
     
     var body: some View {
         HStack {
@@ -20,18 +25,31 @@ struct LandmarkRow: View {
                 
             
             Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
+//            FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
         }
     }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+//    static let modelData = ModelData()
     static var previews: some View {
         Group{
             LandmarkRow(landmark: landmarks[0])
             
             LandmarkRow(landmark: landmarks[1])
+            
+//            LandmarkRow(landmark: modelData.landmarks[0])
+//
+//            LandmarkRow(landmark: modelData.landmarks[1])
         }
         .previewLayout(.fixed(width: 300, height: 70))
+//        .environmentObject(modelData)
 
     }
 }
