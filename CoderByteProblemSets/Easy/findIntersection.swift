c354018402f8221cd4c6af3ac3daf51a31a9587a
Juan Barba <barbajuan11@gmail.com>
2022-11-15 15:13:20 -0800

    Finished findIntersection problem

---
+++ CoderByteProblemSets/Easy/findIntersection.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+func FindIntersection(_ strArr: [String]) -> String {
+
+  // code goes here   
+  // Note: feel free to modify the return type of this function 
+
+  var returnStr = ""
+  let storageSet =  Set(strArr[0].components(separatedBy: ", "))
+
+  let arr2 = strArr[1].components(separatedBy: ", ")
+
+  for number in arr2{
+    if storageSet.contains(number){
+      returnStr +=  returnStr.count == 0 ? number :  "," + number
+    }
+  }
+
+
+
+  return returnStr
+
+}
