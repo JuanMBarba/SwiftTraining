ab59db908fa2ed22cbe362d02b3aaac33f0ebd00
Juan Barba <barbajuan11@gmail.com>
2022-11-15 15:30:06 -0800

    Commented out a different approach that also works

--- CoderByteProblemSets/Easy/findIntersection.swift
+++ CoderByteProblemSets/Easy/findIntersection.swift
@@ -2,22 +2,31 @@ import Foundation
 
 func FindIntersection(_ strArr: [String]) -> String {
 
-  // code goes here   
-  // Note: feel free to modify the return type of this function 
+    // code goes here   
+    // Note: feel free to modify the return type of this function 
 
-  var returnStr = ""
-  let storageSet =  Set(strArr[0].components(separatedBy: ", "))
+    var returnStr = ""
+    let storageSet =  Set(strArr[0].components(separatedBy: ", "))
 
-  let arr2 = strArr[1].components(separatedBy: ", ")
+    let arr2 = strArr[1].components(separatedBy: ", ")
 
-  for number in arr2{
+    for number in arr2{
     if storageSet.contains(number){
-      returnStr +=  returnStr.count == 0 ? number :  "," + number
+        returnStr +=  returnStr.count == 0 ? number :  "," + number
     }
-  }
+    }
+    //Different approach
+    // var resultArr = [String]()
+  
+    // for number in arr2{
+    //     if storageSet.contains(number){
+    //     resultArr.append(number)
+    //     }
+    // }
+
+    // return resultArr.joined(separator: ",")
 
 
-
-  return returnStr
+    return returnStr
 
 }
